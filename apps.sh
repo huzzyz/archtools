e2a5870e1c53703b9e6c433faa856c2936afd8b6
huzzyz <69323208+huzzyz@users.noreply.github.com>
2023-11-13 11:11:11 +0400

    Update apps.sh
    fixing structure

--- apps.sh
+++ apps.sh
@@ -13,56 +13,79 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 
-# Check for the presence of packages.txt and flatpak_packages.txt
-PACKAGES_FILE="packages.txt"
-FLATPAK_PACKAGES_FILE="flatpak_packages.txt"
-INSTALL_PACKAGES=false
-INSTALL_FLATPAK_PACKAGES=false
+install_yay_and_powerpill() {
+    # Install yay if it's not installed
+    if ! command -v yay &> /dev/null; then
+        print_message $YELLOW "Installing yay..."
+        sudo pacman -Sy --needed --noconfirm git base-devel
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si --noconfirm
+        cd ..
+        rm -rf yay
+        print_message $GREEN "yay installed successfully."
+    else
+        print_message $GREEN "yay is already installed."
+    fi
 
-if [ -f "$PACKAGES_FILE" ]; then
-    INSTALL_PACKAGES=true
-else
-    print_message $RED "Warning: $PACKAGES_FILE not found. Skipping Arch/AUR package installation."
-fi
+    # Configure yay to use powerpill
+    print_message $YELLOW "Configuring yay to use powerpill..."
+    yay -Syu --save --noconfirm
+    sed -i 's|"pacmanbin": "pacman"|"pacmanbin": "powerpill"|' ~/.config/yay/config.json
 
-if [ -f "$FLATPAK_PACKAGES_FILE" ]; then
-    INSTALL_FLATPAK_PACKAGES=true
-else
-    print_message $RED "Warning: $FLATPAK_PACKAGES_FILE not found. Skipping Flatpak package installation."
-fi
+    # Install powerpill for faster downloads if not installed
+    if ! command -v powerpill &> /dev/null; then
+        print_message $YELLOW "Installing powerpill..."
+        yay -S --needed --noconfirm powerpill
+        print_message $GREEN "powerpill installed successfully."
+    else
+        print_message $GREEN "powerpill is already installed."
+    fi
+}
 
-# Rest of the script (like checking for AMD GPU, installing yay and powerpill)...
-
-# Install packages using yay
-if [ "$INSTALL_PACKAGES" = true ]; then
-    print_message $YELLOW "Installing Arch/AUR packages from $PACKAGES_FILE..."
-    while IFS= read -r package || [[ -n "$package" ]]; do
-        if ! pacman -Qi $package &> /dev/null; then
-            print_message $YELLOW "Installing package: $package"
-            if ! yay -S --needed --noconfirm $package; then
-                print_message $RED "Error: Failed to install package: $package"
-                exit 1
+install_packages() {
+    # Install packages using yay
+    if [ -f "packages.txt" ]; then
+        print_message $YELLOW "Installing Arch/AUR packages from packages.txt..."
+        while IFS= read -r package || [[ -n "$package" ]]; do
+            if ! pacman -Qi $package &> /dev/null; then
+                print_message $YELLOW "Installing package: $package"
+                if ! yay -S --needed --noconfirm $package; then
+                    print_message $RED "Error: Failed to install package: $package"
+                    exit 1
+                fi
+            else
+                print_message $GREEN "Package already installed: $package"
             fi
-        else
-            print_message $GREEN "Package already installed: $package"
-        fi
-    done < "$PACKAGES_FILE"
-    print_message $GREEN "Arch/AUR packages installed successfully."
-fi
+        done < packages.txt
+        print_message $GREEN "Arch/AUR packages installed successfully."
+    else
+        print_message $RED "packages.txt not found. Skipping Arch/AUR package installation."
+    fi
+}
 
-# Install Flatpak packages
-if [ "$INSTALL_FLATPAK_PACKAGES" = true ]; then
-    print_message $YELLOW "Installing Flatpak packages from $FLATPAK_PACKAGES_FILE..."
-    while IFS= read -r package || [[ -n "$package" ]]; do
-        if ! flatpak list | grep -q $package; then
-            print_message $YELLOW "Installing Flatpak package: $package"
-            if ! flatpak install --noconfirm -y $package; then
-                print_message $RED "Error: Failed to install Flatpak package: $package"
-                exit 1
+install_flatpak_packages() {
+    # Install Flatpak packages
+    if [ -f "flatpak_packages.txt" ]; then
+        print_message $YELLOW "Installing Flatpak packages from flatpak_packages.txt..."
+        while IFS= read -r package || [[ -n "$package" ]]; do
+            if ! flatpak list | grep -q $package; then
+                print_message $YELLOW "Installing Flatpak package: $package"
+                if ! flatpak install --noconfirm -y $package; then
+                    print_message $RED "Error: Failed to install Flatpak package: $package"
+                    exit 1
+                fi
+            else
+                print_message $GREEN "Flatpak package already installed: $package"
             fi
-        else
-            print_message $GREEN "Flatpak package already installed: $package"
-        fi
-    done < "$FLATPAK_PACKAGES_FILE"
-    print_message $GREEN "Flatpak packages installed successfully."
-fi
+        done < flatpak_packages.txt
+        print_message $GREEN "Flatpak packages installed successfully."
+    else
+        print_message $RED "flatpak_packages.txt not found. Skipping Flatpak package installation."
+    fi
+}
+
+# Main script starts here
+install_yay_and_powerpill
+install_packages
+install_flatpak_packages
