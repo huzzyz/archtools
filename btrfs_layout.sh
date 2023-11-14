3afa98da9aed62b1c30b7193d906f6393296b7a1
huzzyz <69323208+huzzyz@users.noreply.github.com>
2023-11-14 14:50:57 +0400

    Update btrfs_layout.sh

--- btrfs_layout.sh
+++ btrfs_layout.sh
@@ -5,21 +5,47 @@ print_message() {
     echo -e "\033[1;32m$1\033[0m" # Green color for messages
 }
 
+# Function to validate the partition or disk
+validate_device() {
+    local device=$1
+    while true; do
+        if [ -b "$device" ]; then
+            break
+        else
+            echo "Invalid device: $device. Please enter a valid device:"
+            read -r device
+        fi
+    done
+    echo "$device"  # Return the valid device name
+}
+
 print_message "WARNING: This script can modify disk partitions and may lead to data loss."
 echo "Please ensure you have backups before proceeding."
 read -p "Press Enter to continue or Ctrl+C to abort."
 
-# Display available disks and partitions
 print_message "Current Disk Layout:"
 lsblk -o NAME,SIZE,TYPE,MOUNTPOINT
 
-# Ask if user wants to create new partitions
+# Partition deletion (optional)
+read -p "Do you want to delete any partitions? (yes/no): " delete_partitions
+if [ "$delete_partitions" == "yes" ]; then
+    echo "Enter the partition you want to delete (e.g., /dev/sda1):"
+    read -r partition
+    partition=$(validate_device "$partition")
+    echo "Deleting partition $partition..."
+    (
+    echo d # Delete a partition
+    echo w # Write changes
+    ) | fdisk "$(echo $partition | sed -r 's/(.*[a-z]).*/\1/')"
+fi
+
+# Partition creation (optional)
 read -p "Do you want to create new partitions? (yes/no): " create_partitions
 if [ "$create_partitions" == "yes" ]; then
     echo "Please enter the disk where you want to create partitions (e.g., /dev/sda):"
     read -r disk
+    disk=$(validate_device "$disk")
 
-    # Partitioning
     echo "Creating partitions on $disk..."
     (
     echo n # Add a new partition (EFI)
@@ -34,16 +60,16 @@ if [ "$create_partitions" == "yes" ]; then
     ) | fdisk $disk
 fi
 
-# Ask for the EFI and Btrfs partitions
+# Format partitions
 echo "Please enter the EFI partition (e.g., /dev/sda1):"
 read -r efi_partition
-echo "Please enter the Btrfs partition (e.g., /dev/sda2):"
-read -r btrfs_partition
-
-# Format partitions
+efi_partition=$(validate_device "$efi_partition")
 echo "Formatting the EFI partition ($efi_partition)..."
 mkfs.fat -F32 "$efi_partition"
 
+echo "Please enter the Btrfs partition (e.g., /dev/sda2):"
+read -r btrfs_partition
+btrfs_partition=$(validate_device "$btrfs_partition")
 echo "Formatting the Btrfs partition ($btrfs_partition)..."
 mkfs.btrfs "$btrfs_partition"
 
@@ -73,7 +99,6 @@ mount "$efi_partition" /mnt/boot/efi
 print_message "Created Btrfs Subvolumes:"
 btrfs subvolume list /mnt
 
-# Reminder for fstab
 print_message "Remember to update /etc/fstab with the new partitions and subvolumes."
 echo "Press Enter to exit."
 read
