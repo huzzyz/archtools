f585846d25a2c97a449f8b46e504978a6af58b0f
huzzyz <69323208+huzzyz@users.noreply.github.com>
2023-11-16 00:09:04 +0400

    Update btrfs_setup.sh

--- btrfs_setup.sh
+++ btrfs_setup.sh
@@ -32,9 +32,13 @@ create_tmp_fstab_file() {
     local tmp_file="/tmp/btrfs_fstab_entries.tmp"
     
     generate_fstab_entry "$btrfs_partition" "/" "subvol=@,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2" > "$tmp_file"
-    # Add other entries
-    # ...
-    
+    generate_fstab_entry "$btrfs_partition" "/home" "subvol=@home,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2" >> "$tmp_file"
+    generate_fstab_entry "$btrfs_partition" "/var/log" "subvol=@log,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2" >> "$tmp_file"
+    generate_fstab_entry "$btrfs_partition" "/var/cache" "subvol=@cache,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2" >> "$tmp_file"
+    generate_fstab_entry "$btrfs_partition" "/.snapshots" "subvol=@snapshots,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2" >> "$tmp_file"
+    generate_fstab_entry "$btrfs_partition" "/var/lib/libvirt/images" "subvol=@libvirt-images,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2" >> "$tmp_file"
+    generate_fstab_entry "$efi_partition" "/efi" "vfat defaults,umask=0077" >> "$tmp_file"
+
     echo "$tmp_file"
 }
 
@@ -50,10 +54,50 @@ print_message "WARNING: This script assumes you have created the partitions and 
 echo "Please ensure you have backups before proceeding."
 read -p "Press Enter to continue or Ctrl+C to abort."
 
-# ... (existing code)
+print_message "Current Disk Layout:"
+lsblk -o NAME,SIZE,TYPE,MOUNTPOINT
+
+# Ask for the EFI and Btrfs partitions
+echo "Please enter the EFI partition (e.g., /dev/sda1):"
+read -r efi_partition
+efi_partition=$(validate_device "$efi_partition")
+
+echo "Please enter the Btrfs partition (e.g., /dev/sda2):"
+read -r btrfs_partition
+btrfs_partition=$(validate_device "$btrfs_partition")
+
+# Format partitions
+echo "Formatting the EFI partition ($efi_partition)..."
+mkfs.fat -F32 "$efi_partition"
+
+echo "Formatting the Btrfs partition ($btrfs_partition)..."
+mkfs.btrfs "$btrfs_partition"
+
+# Mount the Btrfs partition and create subvolumes
+mount "$btrfs_partition" /mnt
+print_message "Creating Btrfs subvolumes..."
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@cache
+btrfs subvolume create /mnt/@snapshots
+btrfs subvolume create /mnt/@libvirt-images
+umount /mnt
 
 # Mount subvolumes and EFI partition
-# ... (existing code)
+print_message "Mounting subvolumes and EFI partition..."
+mount -o subvol=@,defaults,noatime,compress=zstd "$btrfs_partition" /mnt
+mkdir -p /mnt/{efi,home,var/log,var/cache,.snapshots,var/lib/libvirt/images}
+mount -o subvol=@home,defaults,noatime,compress=zstd "$btrfs_partition" /mnt/home
+mount -o subvol=@log,defaults,noatime,compress=zstd "$btrfs_partition" /mnt/var/log
+mount -o subvol=@cache,defaults,noatime,compress=zstd "$btrfs_partition" /mnt/var/cache
+mount -o subvol=@snapshots,defaults,noatime,compress=zstd "$btrfs_partition" /mnt/.snapshots
+mount -o subvol=@libvirt-images,defaults,noatime,compress=zstd "$btrfs_partition" /mnt/var/lib/libvirt/images
+mount "$efi_partition" /mnt/efi
+
+# Display the created subvolumes
+print_message "Created Btrfs Subvolumes:"
+btrfs subvolume list /mnt
 
 # Generate temporary file with fstab entries
 print_message "Generated fstab Entries:"
@@ -63,6 +107,6 @@ tmp_fstab_file=$(create_tmp_fstab_file)
 touch /tmp/btrfs_layout_done
 
 print_message "Entries added to temporary file: $tmp_fstab_file."
-echo "Run the second script to append the entries to /mnt/etc/fstab."
+echo "Run the second script (append_fstab_entries.sh) to append the entries to /mnt/etc/fstab."
 echo "Press Enter to exit."
 read
